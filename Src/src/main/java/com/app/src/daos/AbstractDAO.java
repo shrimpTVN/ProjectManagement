adfba162a6c4780b8fa08e1d6b8cb2981a9c9a60
shrimp <mrshrimptran@gmail.com>
2026-03-02 15:56:30 +0700

    build login feature( get user data when login success)

--- Src/src/main/java/com/app/src/daos/AbstractDAO.java
+++ Src/src/main/java/com/app/src/daos/AbstractDAO.java
@@ -2,15 +2,30 @@ package com.app.src.daos;
 
 import com.app.src.core.DatabaseConnection;
 import com.app.src.daos.BaseDAO;
+import com.app.src.utils.MySQLDatabaseConnection;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 // 2. Abstract DAO (Applying the Template Method Pattern )
 // This class handles database connection boilerplate so your 9 DAOs don't have to repeat it.
 public abstract class AbstractDAO<T> implements BaseDAO<T> {
-    protected Connection getConnection() {
-        // Return your database connection here
-        return DatabaseConnection.getConnection();
+    public Connection getConnection() throws SQLException {
+       Connection connection = (new MySQLDatabaseConnection()).getConnection();
+        connection.setAutoCommit(false);
+        return connection;
     }
+
+    public void closeResource(Statement statement, Connection connection, ResultSet resultSet) throws SQLException {
+        statement.close();
+        if(resultSet!=null){
+            resultSet.close();
+        }
+        connection.commit();
+        connection.rollback();
+    }
+
     // Shared helper methods for executing queries can go here...
 }
