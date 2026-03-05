ccbeeddcee19540eca496c40868ad9fbffa6e939
DOANTAILOC <149085133+DOANTAILOC@users.noreply.github.com>
2026-03-05 14:01:44 +0700

    thay doi UI

--- Src/src/main/java/com/app/src/ProjectManagementApplication.java
+++ Src/src/main/java/com/app/src/ProjectManagementApplication.java
@@ -3,6 +3,7 @@ package com.app.src;
 import com.app.src.controllers.SceneManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 
 import java.io.IOException;
 
@@ -11,11 +12,18 @@ public class ProjectManagementApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
+
+
+
+
+
         stage.setTitle("Project Management Application");
 
         SceneManager sceneManager = SceneManager.getInstance();
         sceneManager.initStage(stage);
         sceneManager.switchScene("/scenes/login.fxml");
+
+
 
 
 //        load the FXML file from the classpath (src/main/resources)
