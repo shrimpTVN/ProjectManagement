af5fa55f931b972f444f88a537eb0f2e2083cd4d
TuAnh <tuanhvole989@gmail.com>
2026-03-11 14:55:24 +0700

    add ui into List.fxml

--- Src/src/main/java/com/app/src/controllers/project/ListController.java
+++ Src/src/main/java/com/app/src/controllers/project/ListController.java
@@ -1,10 +1,12 @@
 package com.app.src.controllers.project;
 
 import com.app.src.models.Project;
+import javafx.scene.control.Hyperlink;
 
 public class ListController implements IProjectDetailSubView {
     @Override
     public void renderData(Project project, String adminName) {
         System.out.println(project.getProjectName() + " " + adminName);
     }
+
 }
