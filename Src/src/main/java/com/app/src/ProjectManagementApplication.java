5ebeeea077fbc94e0cc99bf04521447c8e759ef4
shrimp <mrshrimptran@gmail.com>
2026-03-11 15:32:40 +0700

    define global exception handler

--- Src/src/main/java/com/app/src/ProjectManagementApplication.java
+++ Src/src/main/java/com/app/src/ProjectManagementApplication.java
@@ -1,6 +1,7 @@
 package com.app.src;
 
 import com.app.src.controllers.SceneManager;
+import com.app.src.exceptions.GlobalExceptionHandler;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.text.Font;
@@ -11,22 +12,24 @@ public class ProjectManagementApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        GlobalExceptionHandler.registerDefaultHandler();
 
+        try {
 
+            stage.setTitle("Project Management Application");
 
+            SceneManager sceneManager = SceneManager.getInstance();
+            sceneManager.initStage(stage);
 
-
-
-        stage.setTitle("Project Management Application");
-
-        SceneManager sceneManager = SceneManager.getInstance();
-        sceneManager.initStage(stage);
-
-        sceneManager.switchScene("/scenes/login.fxml");
+            sceneManager.switchScene("/scenes/login.fxml");
 //        sceneManager.switchScene("/scenes/dashboard.fxml");
 
 
 
 
+        } catch (Exception exception) {
+            GlobalExceptionHandler.handle(exception);
+            throw exception;
+        }
     }
 }
