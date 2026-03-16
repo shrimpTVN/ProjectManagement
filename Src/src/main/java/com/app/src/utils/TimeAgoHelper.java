c0a4ec292968b422be9bd4e1b41f53f7bfe764aa
TuAnh <tuanhvole989@gmail.com>
2026-03-16 16:11:12 +0700

    add new file TimeAgoHelper for helping modify Time display

---
+++ Src/src/main/java/com/app/src/utils/TimeAgoHelper.java
@@ -0,0 +1,18 @@
+﻿package com.app.src.utils;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class TimeAgoHelper {
+    public static String toTimeAgo(Date pastDate) {
+        if (pastDate == null) return "Không rõ";
+        long duration = new Date().getTime() - pastDate.getTime();
+        long diffInMinutes = TimeUnit.MILLISECONDS.toMinutes(duration);
+        long diffInHours = TimeUnit.MILLISECONDS.toHours(duration);
+
+        if (diffInMinutes < 1) return "Vừa xong";
+        if (diffInMinutes < 60) return diffInMinutes + " phút trước";
+        if (diffInHours < 24) return diffInHours + " giờ trước";
+        return new java.text.SimpleDateFormat("dd/MM/yyyy").format(pastDate);
+    }
+}
