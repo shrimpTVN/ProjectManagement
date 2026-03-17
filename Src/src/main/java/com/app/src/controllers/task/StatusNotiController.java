aed3006b113e0e1bcf5cd058b9d0fcfc10646da7
TuAnh <tuanhvole989@gmail.com>
2026-03-17 21:31:42 +0700

    add new file .fxml for being container of many statuses

---
+++ Src/src/main/java/com/app/src/controllers/task/StatusNotiController.java
@@ -0,0 +1,15 @@
+﻿package com.app.src.controllers.task;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StatusNotiController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
